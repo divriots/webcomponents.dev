6127be863bb61d30669d88a3651abc045f84ba37
Yiyi Sun <yiyisun@gmail.com>
2020-12-27 13:25:12 -0500

    add AppRun (JavaScript) template

---
+++ apprun-js/src/index.jsx
@@ -0,0 +1,38 @@
+// http://github.com/yysun/apprun
+// https://apprun.js.org/#play/9
+
+/** @jsx app.h */
+import { app, Component } from "apprun";
+
+const styles = `.my-counter * {
+  font-size: 200%;
+}
+
+.my-counter span {
+  width: 4rem;
+  display: inline-block;
+  text-align: center;
+}
+
+.my-counter button {
+  width: 4rem;
+  height: 4rem;
+  border: none;
+  border-radius: 10px;
+  background-color: seagreen;
+  color: white;
+}`;
+
+const add = (state, num) => state + num;
+
+class Counter extends Component {
+  state = 0;
+  view = state => <div class="my-counter">
+    <style>{styles}</style>
+    <button $onclick={[add, -1]}>-</button>
+    <span>{state}</span>
+    <button $onclick={[add, +1]}>+</button>
+  </div>;
+}
+
+app.webComponent('my-counter', Counter);
